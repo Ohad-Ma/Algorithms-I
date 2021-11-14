acecdd9f9a56451cae122379d7c8b92189347f74
Ohad-Ma <maday.ohad@gmail.com>
2021-11-14 13:02:45 +0200

    base case added

---
+++ SubSetSum.java
@@ -0,0 +1,7 @@
+/**
+ * Problem: Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum. 
+ * 
+ */
+class SubSetSum{
+    
+}
