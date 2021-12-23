c490cca6cf8dc1b612de99373c47181a4d1d1214
Ohad-Ma <maday.ohad@gmail.com>
2021-12-23 14:53:15 +0200

    Updated

--- UnionFind.java
+++ UnionFind.java
@@ -8,6 +8,64 @@
  * 
  * 
  */
+// A union by rank and path compression
 public class UnionFind {
+    /**
+     * Basic graph class
+     */
+    class Graph{
+        int V,E;
+        Edge[] edge;
+
+        //#region constructor
+        Graph(int V,int E){
+            this.V = V;
+            this.E = E;
+            this.edge = new Edge[E];
+            for(int i = 0 ; i < E; i++){
+                edge[i] = new Edge();
+            }
+        }
+        //#endregion
+        //#region inner class for Edges
+        class Edge{
+            int src,dest;
+        }
+        //#endregion
+        //#region basic Subset inner class
+        class Subset{
+            int parent,rank;
+        }
+        //#endregion
+    
+        /**
+         * Find method
+         * @return node's parent
+         */
+        int find(Subset[] subsets, int i){
+            if(subsets[i].parent != i){
+                subsets[i].parent = find(subsets, subsets[i].parent);
+            }
+            return subsets[i].parent;
+        }
+
+        /**
+         * Union two disjoint sets using path compression
+         * @param subsets
+         * @param A
+         * @param B
+         */
+        void Union(Subset[] subsets, int A, int B){
+            int aRoot = find(subsets, A), bRoot = find(subsets, B);
+
+            if(subsets[aRoot].rank < subsets[bRoot].rank) subsets[aRoot].parent = bRoot;
+            else if(subsets[bRoot].rank < subsets[aRoot].rank) subsets[bRoot].parent = aRoot;
+            else {
+                subsets[aRoot].parent = bRoot;
+                subsets[bRoot].rank++;
+            }
+        }
+    
+    }
     
 }
